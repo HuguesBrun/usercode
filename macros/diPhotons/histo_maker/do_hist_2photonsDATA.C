49c0de15acab2a1df825528e9a3d1ca7f59f73e1
hbrun <>
2010-10-15 17:20:47 +0000

    Les macros qui permettent de faire les histo diphotons

---
+++ macros/diPhotons/histo_maker/do_hist_2photonsDATA.C
@@ -0,0 +1,122 @@
+#include "TString.h"
+#include "TH1F.h"
+#include "TChain.h"
+#include "TFile.h"
+#include "TSystem.h"
+#include <iostream>
+
+
+TChain *chain = new TChain("diPhotons");
+TH1F *th1fMaker(TString nom, TString variable, float Xmin, float Xmax, float bining, TString Cuts,  TString nomEnPlus, TString cutEnPlus ){
+	TString theCut;
+	/*if (part==1) {theCut = "pho_isEB==1"; nom+="_EB";}
+	else if (part==2) {theCut = "pho_isEE==1"; nom+="_EE";} 
+	else {theCut = "1"; nom+="_All";}*/
+	theCut += Cuts+"&&"+cutEnPlus;
+	//nom+="_"+nomEnPlus;
+	TH1F *over = new TH1F("over","",(10000-Xmax), Xmax, 10000);
+	TString plotOver = variable+">>over";
+	chain->Draw(plotOver, theCut);
+	int overFlow = over->Integral();
+	delete over;
+	float Lbin = (int) (Xmax - Xmin)/bining;
+	float theLast = Xmax-Lbin;
+	TH1F *h = new TH1F(nom,"",bining, Xmin, Xmax);
+	TString toDraw = variable + ">>" + nom;
+	chain->Draw(toDraw,theCut);
+	h->Fill(theLast,overFlow);
+	return h;
+}
+
+int doHistoGeneTroisCut(TString nom, TString variable, float Xmin, float Xmax, float bining, TString Cuts,  TFile *theFile){
+	cout << "processing " << nom << endl;
+	TH1F *data = (TH1F*) th1fMaker(nom,variable,Xmin,Xmax,bining,Cuts, "1", "1");
+ 	theFile->Write();
+	delete data;
+}
+
+int do_hist_2photonsDATA(){
+  TFile *myFile = new TFile("diphoFile_DATA.root","RECREATE");
+   chain->Add("diphoton_part*.root");
+  
+TString baseCut = "event_nVertex==1&&(abs(pholead_eta)<=2.5)&&(!((abs(pholead_eta)>1.4442)&&(abs(pholead_eta)<1.566)))&&(abs(pholead_eta)<=2.5)&&(!((abs(pholead_eta)>1.4442)&&(abs(pholead_eta)<1.566)))&&pholead_hoe<0.05&&photrail_hoe<0.05&&pholead_pt>30&&photrail_pt>30";
+TString isoLead = "(pholead_TrackerIso<2&&pholead_EcalIso<4.2&&pholead_HcalIso<2.2)";
+TString isoTrail = "(photrail_TrackerIso<2&&photrail_EcalIso<4.2&&photrail_HcalIso<2.2)";
+TString cutIeta = "(((pholead_isEB==1)&&(pholead_sigieta<0.01))||((pholead_isEE==1)&&(pholead_sigieta<0.03)))&&(((photrail_isEB==1)&&(photrail_sigieta<0.01))||((photrail_isEE==1)&&(photrail_sigieta<0.03)))";
+TString pixelCut = "pholead_HasPixSeed==0&&photrail_HasPixSeed==0";
+TString combisoLead = "((pholead_TrackerIso+pholead_EcalIso+pholead_HcalIso)<3.0)";
+TString combisoTrail = "((photrail_TrackerIso+photrail_EcalIso+photrail_HcalIso)<3.0)";
+
+TString noIetaPixel = baseCut + "&&" + pixelCut;
+doHistoGeneTroisCut("noIetaPixel","dipho_mgg",0,510,34,noIetaPixel, myFile);
+
+TString baseCutPixel = baseCut+"&&"+pixelCut+"&&" +cutIeta;
+doHistoGeneTroisCut("baseCutPixel","dipho_mgg",0,510,34,baseCutPixel, myFile);
+
+TString isoLeadPixel = baseCut+"&&"+pixelCut+"&&"+isoLead+"&&" +cutIeta;
+doHistoGeneTroisCut("leadSelecPixel","dipho_mgg",0,519,34,isoLeadPixel, myFile);
+
+TString isoTrailPixel = baseCut+"&&"+pixelCut+"&&"+isoTrail+"&&" +cutIeta;
+doHistoGeneTroisCut("trailSelecPixel","dipho_mgg",0,510,34,isoTrailPixel, myFile);
+
+TString isoOnePixel = baseCut+"&&"+pixelCut+"&&("+isoLead + "||" +isoTrail+")"+"&&" +cutIeta;
+doHistoGeneTroisCut("oneSelecPixel","dipho_mgg",0,510,34,isoOnePixel, myFile);
+
+TString isoBothPixel = baseCut+"&&"+pixelCut+"&&("+isoLead + "&&" +isoTrail+")"+"&&" +cutIeta;
+doHistoGeneTroisCut("bothSelecPixel","dipho_mgg",0,510,34,isoBothPixel, myFile);
+
+TString combisoLeadPixel  =baseCut + "&&" + pixelCut + "&&" + combisoLead+"&&" +cutIeta;
+doHistoGeneTroisCut("leadCombIsoPixel","dipho_mgg",0,510,34,combisoLeadPixel, myFile);
+
+TString combisoTrailPixel  =baseCut + "&&" + pixelCut + "&&" + combisoTrail+"&&" +cutIeta;
+doHistoGeneTroisCut("trailCombIsoPixel","dipho_mgg",0,510,34,combisoTrailPixel, myFile);
+
+TString combisoOnePixel  =baseCut + "&&" + pixelCut + "&&(" + combisoLead+"||" + combisoTrail + ")&&" +cutIeta;
+doHistoGeneTroisCut("oneCombIsoPixel","dipho_mgg",0,510,34,combisoOnePixel, myFile);
+
+TString combisoBothPixel  =baseCut + "&&" + pixelCut + "&&(" + combisoLead+"&&" + combisoTrail + ")&&" +cutIeta;
+doHistoGeneTroisCut("bothCombIsoPixel","dipho_mgg",0,510,34,combisoBothPixel, myFile);
+
+///////////////////////////////////////////////////Now with no pixel !!
+
+TString noIeta = baseCut;
+doHistoGeneTroisCut("noIetaNoPixel","dipho_mgg",0,510,102,baseCut, myFile);
+
+TString baseCut = baseCut+"&&" +cutIeta;
+doHistoGeneTroisCut("baseCutNoPixel","dipho_mgg",0,510,102,baseCut, myFile);
+
+TString isoLead = baseCut+"&&"+isoLead+"&&" +cutIeta;
+doHistoGeneTroisCut("leadSelecNoPixel","dipho_mgg",0,510,102,isoLead, myFile);
+
+TString isoTrail = baseCut+"&&"+isoTrail+"&&" +cutIeta;
+doHistoGeneTroisCut("trailSelecNoPixel","dipho_mgg",0,510,102,isoTrail, myFile);
+
+TString isoOne = baseCut+"&&("+isoLead + "||" +isoTrail+")"+"&&" +cutIeta;
+doHistoGeneTroisCut("oneSelecNoPixel","dipho_mgg",0,510,102,isoOne, myFile);
+
+TString isoBoth = baseCut+"&&("+isoLead + "&&" +isoTrail+")"+"&&" +cutIeta;
+doHistoGeneTroisCut("bothSelecNoPixel","dipho_mgg",0,510,102,isoBoth, myFile);
+
+TString combisoLead =baseCut + "&&" + combisoLead+"&&" +cutIeta;
+doHistoGeneTroisCut("leadCombIsoNoPixel","dipho_mgg",0,510,102,combisoLead, myFile);
+
+TString combisoTrail  =baseCut + "&&" +  combisoTrail+"&&" +cutIeta;
+doHistoGeneTroisCut("trailCombIsoNoPixel","dipho_mgg",0,510,102,combisoTrail, myFile);
+
+TString combisoOne  =baseCut + "&&(" + combisoLead+"||" + combisoTrail + ")&&" +cutIeta;
+doHistoGeneTroisCut("oneCombIsoNoPixel","dipho_mgg",0,510,102,combisoOne, myFile);
+
+TString combisoBoth  =baseCut + "&&(" + combisoLead+"&&" + combisoTrail + ")&&" +cutIeta;
+doHistoGeneTroisCut("bothCombIsoNoPixel","dipho_mgg",0,510,102,combisoBoth, myFile);
+
+TString combisoBothWithPixel  =baseCut + "&&(" + combisoLead+"&&" + combisoTrail + ")&&" +cutIeta + "&&pholead_HasPixSeed==1&&photrail_HasPixSeed==1";
+doHistoGeneTroisCut("bothCombIsoWithPixel","dipho_mgg",0,510,102,combisoBothWithPixel, myFile);
+
+
+// do a plot for the Z fit !!!
+
+doHistoGeneTroisCut("bothCombIsoWithPixelZoom","dipho_mgg",70,110,30,combisoBothWithPixel, myFile);
+
+
+
+}
---
+++ macros/diPhotons/histo_maker/do_hist_distDATA.C
@@ -0,0 +1,97 @@
+#include "TString.h"
+#include "TH1F.h"
+#include "TChain.h"
+#include "TFile.h"
+#include "TSystem.h"
+#include <iostream>
+
+
+TChain *chain = new TChain("diPhotons");
+TH1F *th1fMaker(TString nom, TString variable, float Xmin, float Xmax, float bining, TString Cuts,  TString nomEnPlus, TString cutEnPlus ){
+	TString theCut;
+	/*if (part==1) {theCut = "pho_isEB==1"; nom+="_EB";}
+	else if (part==2) {theCut = "pho_isEE==1"; nom+="_EE";} 
+	else {theCut = "1"; nom+="_All";}*/
+	theCut += Cuts+"&&"+cutEnPlus;
+	//nom+="_"+nomEnPlus;
+	TH1F *over = new TH1F("over","",(10000-Xmax), Xmax, 10000);
+	TString plotOver = variable+">>over";
+	chain->Draw(plotOver, theCut);
+	int overFlow = over->Integral();
+	delete over;
+	float Lbin = (int) (Xmax - Xmin)/bining;
+	float theLast = Xmax-Lbin;
+	TH1F *h = new TH1F(nom,"",bining, Xmin, Xmax);
+	TString toDraw = variable + ">>" + nom;
+	chain->Draw(toDraw,theCut);
+	h->Fill(theLast,overFlow);
+	return h;
+}
+
+int doHistoGeneTroisCut(TString nom, TString variable, float Xmin, float Xmax, float bining, TString Cuts,  TFile *theFile){
+	cout << "processing " << nom << endl;
+	TH1F *data = (TH1F*) th1fMaker(nom,variable,Xmin,Xmax,bining,Cuts, "1", "1");
+ 	theFile->Write();
+	delete data;
+}
+
+int do_hist_distDATA(){
+  TFile *myFile = new TFile("diphoFile_DATA.root","RECREATE");
+   chain->Add("diphoton_part*.root");
+  
+TString baseCut = "(abs(pholead_eta)<=2.5)&&(!((abs(pholead_eta)>1.4442)&&(abs(pholead_eta)<1.566)))&&(abs(pholead_eta)<=2.5)&&(!((abs(pholead_eta)>1.4442)&&(abs(pholead_eta)<1.566)))&&pholead_hoe<0.05&&photrail_hoe<0.05&&pholead_pt>30&&photrail_pt>30";
+TString isoLead = "(pholead_TrackerIso<2&&pholead_EcalIso<4.2&&pholead_HcalIso<2.2)";
+TString isoTrail = "(photrail_TrackerIso<2&&photrail_EcalIso<4.2&&photrail_HcalIso<2.2)";
+TString cutIeta = "(((pholead_isEB==1)&&(pholead_sigieta<0.01))||((pholead_isEE==1)&&(pholead_sigieta<0.03)))&&(((photrail_isEB==1)&&(photrail_sigieta<0.01))||((photrail_isEE==1)&&(photrail_sigieta<0.03)))";
+TString pixelCut = "pholead_HasPixSeed==0&&photrail_HasPixSeed==0";
+TString combisoLead = "((pholead_TrackerIso+pholead_EcalIso+pholead_HcalIso)<3.0)";
+TString combisoTrail = "((photrail_TrackerIso+photrail_EcalIso+photrail_HcalIso)<3.0)";
+
+TString noIetaPixel = cutIeta + "&&" + pixelCut;
+
+doHistoGeneTroisCut("leadEt_noIso","pholead_pt",0,150,30,noIetaPixel, myFile);
+doHistoGeneTroisCut("trailEt_noIso","photrail_pt",0,150,30,noIetaPixel, myFile);
+
+doHistoGeneTroisCut("isoTrackerLead","pholead_TrackerIso",-1,20,42,noIetaPixel, myFile);
+doHistoGeneTroisCut("isoTrackerTrail","photrail_TrackerIso",-1,20,42,noIetaPixel, myFile);
+
+doHistoGeneTroisCut("isoEcalLead","pholead_EcalIso",-2,16,36,noIetaPixel, myFile);
+doHistoGeneTroisCut("isoEcalTrail","photrail_EcalIso",-2,16,36,noIetaPixel, myFile);
+
+doHistoGeneTroisCut("isoHcalLead","pholead_HcalIso",-1,10,22,noIetaPixel, myFile);
+doHistoGeneTroisCut("isoHcalTrail","photrail_HcalIso",-1,10,22,noIetaPixel, myFile);
+
+doHistoGeneTroisCut("sumIsoLead","(pholead_TrackerIso+pholead_EcalIso+pholead_HcalIso)",-3,40,43,noIetaPixel, myFile);
+doHistoGeneTroisCut("sumIsoTrail","(photrail_TrackerIso+photrail_EcalIso+photrail_HcalIso)",-3,40,43,noIetaPixel, myFile);
+
+doHistoGeneTroisCut("cosThetaStarLead_noIso","dipho_costhetastar",-1,1,20,noIetaPixel, myFile);
+
+doHistoGeneTroisCut("etaStar_noIso","dipho_etastar",-3,3,24,noIetaPixel, myFile);
+
+doHistoGeneTroisCut("qt_noIso","dipho_qt",0,150,30,noIetaPixel, myFile);
+
+doHistoGeneTroisCut("ql_noIso","dipho_ql",-750,750,30,noIetaPixel, myFile);
+
+
+////////////////////////////////////////////////////////////////////// after isolation cuts
+TString ietaIso = cutIeta +"&&" + isoLead+ "&&" + isoTrail + "&&" + pixelCut;
+doHistoGeneTroisCut("leadEt_iso","pholead_pt",0,150,30,ietaIso, myFile);
+doHistoGeneTroisCut("trailEt_iso","photrail_pt",0,150,30,ietaIso, myFile);
+doHistoGeneTroisCut("cosThetaStarLead_iso","dipho_costhetastar",-1,1,20,ietaIso, myFile);
+
+doHistoGeneTroisCut("etaStar_iso","dipho_etastar",-3,3,24,ietaIso, myFile);
+doHistoGeneTroisCut("qt_iso","dipho_qt",0,150,30,ietaIso, myFile);
+doHistoGeneTroisCut("ql_iso","dipho_ql",-750,750,30,ietaIso, myFile);
+
+
+////////////////////////////////////////////////////////////////////// after isolation cuts
+TString ietaCombIso = cutIeta +"&&" + isoLead+ "&&" + isoTrail + "&&" + pixelCut;
+doHistoGeneTroisCut("leadEt_combIso","pholead_pt",0,150,30,ietaCombIso, myFile);
+doHistoGeneTroisCut("trailEt_combIso","photrail_pt",0,150,30,ietaCombIso, myFile);
+doHistoGeneTroisCut("cosThetaStarLead_combIso","dipho_costhetastar",-1,1,20,ietaCombIso, myFile);
+
+doHistoGeneTroisCut("etaStar_combIso","dipho_etastar",-3,3,24,ietaCombIso, myFile);
+doHistoGeneTroisCut("qt_combIso","dipho_qt",0,150,30,ietaCombIso, myFile);
+doHistoGeneTroisCut("ql_combIso","dipho_ql",-750,750,30,ietaCombIso, myFile);
+
+}
